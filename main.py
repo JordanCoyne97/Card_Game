edd4c06013574e905d270ce1246dbeafd137d2b1
unknown <jordancoyne@hotmail.com>
2020-11-08 11:24:12 +0000

    hi neil

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def card_count_left_in_deck():
 
 
 def get_king_count():
-    return np.count_nonzero(deck == 'King')
+    counter = 0
+
+    for card in deck:
+        if card == 'King of Hearts' or card == 'King of Spades' or card == 'King of Diamonds' or card == 'King of Clubs':
+            counter = counter + 1
+            print('found')
+
+    if 'King of Hearts' in deck:
+        counter = counter + 1
+
+    return counter
 
 
 values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
